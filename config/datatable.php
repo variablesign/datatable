8b7aeb805c0a9496c10558eccbe7253208d56664
Variable Sign <variablesign@gmail.com>
2022-06-23 10:13:45 +0000

    minor additions

--- config/datatable.php
+++ config/datatable.php
@@ -1,8 +1,43 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Per Page
+    |--------------------------------------------------------------------------
+    |
+    | The number of records to display per page in a table.
+    |
+    */
+
+    'per_page' => 10,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Maximum Per Page
+    |--------------------------------------------------------------------------
+    |
+    | The maximum number of records to display per page.
+    |
+    */
+
+    'max_per_page' => 50,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Per Page Options
+    |--------------------------------------------------------------------------
+    |
+    | Options for number of records to display per page.
+    | The maximum value should have the same value as 'max_per_page'.
+    |
+    */
+
+    'per_page_options' => [
+        10 => 'Show 10 Entries', 
+        25 => 'Show 25 Entries', 
+        50 => 'Show 50 Entries'
+    ],
+    
 ];
--- src/Contracts/Datatable.php
+++ src/Contracts/Datatable.php
@@ -3,7 +3,6 @@
 namespace VariableSign\Datatable\Contracts;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Arr;
 
 abstract class Datatable
 {
@@ -31,28 +30,54 @@ abstract class Datatable
     /**
      * The number of records to display per page.
      *
-     * @var int
+     * @var null|int
      */
-    protected $perPage = 10;
+    protected $perPage = null;
 
     /**
      * The maximum number of records to display per page.
      *
-     * @var int
+     * @var null|int
      */
-    protected $maxPerPage = 50;
+    protected $maxPerPage = null;
+
+    /**
+     * Option for number of records to display per page.
+     *
+     * @var null|array
+     */
+    protected $perPageOptions = null;
 
     /**
      * Set a custom placeholder text for the search input.
      *
-     * @var string
+     * @var null|string
      */
     protected $searchLanguage = null;
 
+    /**
+     * @var array
+     */
     private $searchable = [];
+
+    /**
+     * @var string
+     */
     private $search;
+
+    /**
+     * @var string
+     */
     private $sort;
+
+    /**
+     * @var object
+     */
     private $query;
+
+    /**
+     * @var bool
+     */
     private $total;
 
     public function __construct()
@@ -60,24 +85,34 @@ abstract class Datatable
         $this->total = request('get_total') ? true : false;
         $this->search = request('search');
         $this->sort = request('sort_column') ?? $this->sortColumn;
-        $this->perPage = request('per_page') ?? config('settings.paginate', $this->perPage);
+        $this->perPage = request('per_page') ?? $this->perPage ?? config('datatable.per_page', 10);
+        $this->maxPerPage = $this->maxPerPage ?? config('datatable.max_per_page', 50);
         $this->perPage = ($this->perPage > $this->maxPerPage) ? $this->maxPerPage : $this->perPage;
+        $this->perPageOptions = $this->perPageOptions ?? config('datatable.per_page_options', []);
         $this->sortDirection = request('sort_direction') ?? $this->sortDirection;
         $this->sortColumn = $this->sortColumns($this->sort) ?? $this->sortColumn;
         $this->searchable = $this->searchColumns();
         $this->query = $this->query();
+        $this->checkErrors();
     }
 
     final public function render(?string $view = null, array $data = []): mixed
     {
         $results = $this->query->when($this->search, function ($query) {
             return $query->where(function ($query) {
+                $terms = explode(' ', $this->search);
+                foreach ($this->searchable ?? [] as $column) {
+                    $query->orWhereIn($column, $terms);
+                }
+
                 foreach ($this->searchable ?? [] as $column) {
                     $query->orWhere($column,'like','%'.$this->search.'%');
                 }
             });
         })
-        ->orderBy($this->sortColumn, $this->sortDirection);
+        ->when($this->sortColumn, function ($query) {
+            return $query->orderBy($this->sortColumn, $this->sortDirection);
+        });
 
         // Return total records only
         if ($this->total === true) {
@@ -89,11 +124,14 @@ abstract class Datatable
                 'last_page' => ($lastPage <= 0) ? 1 : $lastPage
             ]);
         } 
-        
+
         // Paginate
         $allData = [
             'paginator' => $results->simplePaginate($this->perPage)->withQueryString(),
             'columns' => $this->headerColumns(),
+            'searchable' => empty($this->searchColumns()) ? false : $this->searchColumns(),
+            'sortable' => empty($this->sortColumns()) ? false : array_values($this->sortColumns()),
+            'per_page_options' => $this->perPageOptions,
             'language' => [
                 'search' => $this->getSearchLang()
             ]
@@ -162,8 +200,13 @@ abstract class Datatable
         }
 
         $values = $this->searchColumns(true);
+
+        $values = array_map(function($value) {
+            return str_replace(['_', '-'], ' ', $value);
+        }, $values);
+
         $total = count($values);
-        $last = array_pop($values);
+        $last = ($total <= 1) ?: array_pop($values);
 
         if ($total == 0) {
             return 'Search ' . $this->name . '...';
@@ -178,6 +221,21 @@ abstract class Datatable
         $name = 'Search ' . $this->name . ' by ';
 
         return $name . implode(', ', $values) . $last;
+    }
+
+    private function checkErrors()
+    {
+        if (!is_null($this->perPageOptions) && !is_array($this->perPageOptions)) {
+            throw new \Exception("The perPageOptions property must be an array or null, ".gettype($this->perPageOptions)." given");
+        }
+
+        if (!is_null($this->maxPerPage) && !is_int($this->maxPerPage)) {
+            throw new \Exception("The maxPerPage property must be an integer or null, ".gettype($this->maxPerPage)." given");
+        }
+
+        if ($this->sortColumn == null) {
+            throw new \Exception("The default sortable column cannot be null");
+        }
     }
 
     abstract protected function query(): Builder;
